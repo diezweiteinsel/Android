de8de38c7103968a36ba36187a723555a2504f45
maxratjen <stu246099@mail.uni-kiel.de>
2025-09-22 16:01:22 +0200

    feat. options screen UI, logic missing

---
+++ android/app/src/main/java/de/cau/inf/se/sopro/ui/OptionsScreen/OptionsScreenViewModel.kt
@@ -0,0 +1,38 @@
+package de.cau.inf.se.sopro.ui.OptionsScreen
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import de.cau.inf.se.sopro.R
+
+@Composable
+
+
+fun ChangeURLTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    label: @Composable (() -> Unit)? = null
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        label = label,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        singleLine = true
+    )
+}
+
+@Composable
+fun SaveButton(onClick: () -> Unit) {
+    ElevatedButton(onClick = { onClick() }) {
+        Text(stringResource(R.string.options_save_button))
+
+    }
+}
