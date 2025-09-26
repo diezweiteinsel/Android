352410054741d9af7f560b1755bde004612f4a50
maxratjen <stu246099@mail.uni-kiel.de>
2025-09-26 17:16:14 +0200

    forgot ApplicantDao

--- android/app/src/main/java/de/cau/inf/se/sopro/persistence/dao/ApplicantDao.kt
+++ android/app/src/main/java/de/cau/inf/se/sopro/persistence/dao/ApplicantDao.kt
@@ -4,20 +4,22 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.TypeConverter
+import androidx.room.Upsert
 import de.cau.inf.se.sopro.model.applicant.Applicant
 import de.cau.inf.se.sopro.model.applicant.Usertype
 
 @Dao
 interface ApplicantDao {
 
-    @Query("Select jwt from applicant where userid =:userid")
-    fun getJwt(userid: Int) : String?
+    @Query("Select jwt from applicant where userid = :userId")
+    fun getJwt(userId: Int) : String?
 
 
     @Query("Select userid from applicant")
-    fun getUserId() : List<Int>
-    @Insert
-    suspend fun saveJwt(jwt : List<String>)
+    fun getUserId() : Int
+    @Upsert
+    suspend fun saveJwt(applicant: Applicant)
 
     @Insert(onConflict = OnConflictStrategy.IGNORE) //we want to be able to create users with the same username and password
     suspend fun saveApplicant(applicant: Applicant)
